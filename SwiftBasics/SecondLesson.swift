c60e1a79f565f282d38ab50094e96356e0a6ba08
Bryle Sanico <i_bienebryle.sanico@stratpoint.com>
2025-03-20 21:45:51 +0800

    Second Lesson: Shapes

--- SwiftBasics/SecondLesson.swift
+++ SwiftBasics/SecondLesson.swift
@@ -9,7 +9,34 @@ import SwiftUI
 
 struct SecondLesson: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Circle()
+//            .fill(Color.blue)
+//            .foregroundStyle(Color.pink)
+//            .stroke()
+//            .stroke(Color.red, lineWidth: 10)
+            .stroke(Color.pink, style: StrokeStyle(lineWidth: 30, lineCap: .butt, dash: [10]))
+//            .trim(from: 0.3, to: 1.0)
+//            .stroke(Color.purple, lineWidth: 10)
+        
+        
+        Ellipse()
+            .trim(from: 0.5, to: 1.0)
+            .fill(Color.red)
+            .frame(width: 200, height: 100, alignment: .center)
+            .padding(20)
+
+        
+        Capsule(style: .circular)
+            .fill(Color.yellow)
+            .frame(width: 200, height: 100, alignment: .center)
+        
+        Rectangle()
+            .fill(Color.green)
+            .frame(width: 200, height: 100)
+        
+        RoundedRectangle(cornerRadius: 20)
+            .foregroundStyle(Color.purple)
+            .frame(width: 200, height: 100)
     }
 }
 
