d3668c96b890a465dc32cd5b97f08285434065de
Bryle Sanico <i_bienebryle.sanico@stratpoint.com>
2025-03-20 14:33:17 +0800

    Initial Commit

---
+++ SwiftBasics/SwiftBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftBasicsApp.swift
+//  SwiftBasics
+//
+//  Created by Biene Bryle Sanico on 3/20/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
