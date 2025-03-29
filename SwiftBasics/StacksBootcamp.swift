d6ade7cbc872f233d72e50caf457df85ae882ec0
Bryle Sanico <i_bienebryle.sanico@stratpoint.com>
2025-03-29 22:27:37 +0800

    StacksBootcamp

---
+++ SwiftBasics/StacksBootcamp.swift
@@ -0,0 +1,36 @@
+//
+//  EightLesson.swift
+//  SwiftBasics
+//
+//  Created by Biene Bryle Sanico on 3/29/25.
+//
+
+import SwiftUI
+
+struct EightLesson: View {
+    
+    // HStacks -> Horizontal
+    // VStacks -> Vertical
+    // ZStacks -> zIndex (back to front)
+    var body: some View {
+        VStack {
+            Rectangle()
+                .fill(Color.blue)
+                .frame(width: 100, height: 100)
+            
+            Rectangle()
+                .fill(Color.red)
+                .frame(width: 100, height: 100)
+            
+            Rectangle()
+                .fill(Color.green)
+                .frame(width: 100, height: 100)
+            
+                
+        }
+    }
+}
+
+#Preview {
+    EightLesson()
+}
